cd7b760b6ccc7f0454f6befe3d0a5fc9ff501e25
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-05-30 23:16:32 +0530

    Styling

---
+++ Todo List/todo/src/Components/Balloon.jsx
@@ -0,0 +1,9 @@
+
+
+
+export const Balloon = () => {
+
+    return(
+        
+    )
+}
--- Todo List/todo/src/Components/Todo.jsx
+++ Todo List/todo/src/Components/Todo.jsx
@@ -4,11 +4,26 @@ export const Todo = () => {
 
     const [todo , setTodo] = useState("")
     const [items , setItems] = useState([])
+    const [flag , setFlag] = useState(false);
 
     const addItems = () => {
         setItems([...items , todo])
     }
     console.log(items)
+
+
+    // For Delete ------------------------------------------------>
+
+    const Delete = (index) => {
+        items.splice(index,1)
+        setItems([...items])
+    }
+
+    // For Edit ---------------------------------------------------->
+
+    const Edit = () => {
+        
+    }
 
     return(
         <>
@@ -18,11 +33,19 @@ export const Todo = () => {
             <label> Todo : </label>
             <input type="text" placeholder="enter your todo" onChange={(e)=> setTodo(e.target.value)} />
             <button onClick={addItems}>Submit</button>
+
+            {items.map((el,index) => (
+               <div key={index}>
+                   <p>{index+1}. {el} </p> 
+                   <button onClick={()=>Delete(index)}>Delete</button>
+                   <button onClick={()=>Edit(index)}>Edit</button>
+               </div>
+              
+            ))}
+
         </div>
 
-        {items.map(el => (
-           <p>{el}</p>
-        ))}
+       
 
         </>
     )
