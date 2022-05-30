a224a966e2050ad2a23b9769183cad5cb13810d4
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-05-30 17:59:23 +0530

    Styling

--- Todo List/todo/src/Components/Todo.jsx
+++ Todo List/todo/src/Components/Todo.jsx
@@ -1,12 +1,29 @@
-
+import { useState } from "react"
 
 export const Todo = () => {
 
+    const [todo , setTodo] = useState("")
+    const [items , setItems] = useState([])
+
+    const addItems = () => {
+        setItems([...items , todo])
+    }
+    console.log(items)
 
     return(
         <>
+
         <h1>TODO LIST</h1>
-        <div className="todo"></div>
+        <div className="todo">
+            <label> Todo : </label>
+            <input type="text" placeholder="enter your todo" onChange={(e)=> setTodo(e.target.value)} />
+            <button onClick={addItems}>Submit</button>
+        </div>
+
+        {items.map(el => (
+           <p>{el}</p>
+        ))}
+
         </>
     )
 }
