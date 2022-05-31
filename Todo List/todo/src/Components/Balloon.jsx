0916e37823a36eef37d9c5065a616863c32fe6c0
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-05-31 20:01:11 +0530

    Balloon

--- Todo List/todo/src/Components/Balloon.jsx
+++ Todo List/todo/src/Components/Balloon.jsx
@@ -1,14 +1,50 @@
+import { useState } from "react";
+import { useEffect } from "react";
 
 
 
 export const Balloon = () => {
 
+    const [color , setColor] = useState([]);
+
+
+    // ------------------- For generating Random Colors ----------------------------------------->
+
+
+    const RandomColor = () => {
+        
+        var color = "#" + Math.floor(Math.random()*16777215).toString(16);
+        return color;
+    }
+
+
+
+    // --------------------- Random Colors Arrray ------------------------------------------------>
+
+
+    const RandomColorArray = () => {
+
+        var colorArr = Array(5)
+        .fill()
+        .map( ( _,index) => ({ id : index , color : RandomColor()}))
+        setColor([...colorArr])
+    }
+
+    useEffect(() => {RandomColorArray()},[])
+
     return(
         <>
-      <h1>BALLOON GAME</h1>
-      <div className="mainDiv">
+
+        <h1>BALLOON GAME</h1>
+
+        <div className="mainDiv">
             <div className="left"></div>
-            <div className="right"></div>
+            <div className="right">
+                {color.map((el)=>(
+                    <div style={{backgroundColor : el.color}} key={el.id} >
+                    </div>
+                ))}
+            </div>
             <div className="input">
                 <input type="text" id="ballonnumber" placeholder="Enter Your Ballon Number"  /> <br />
                 <button>Shoot</button>
