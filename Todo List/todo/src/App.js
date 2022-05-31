d2be1f34710399e8e9da91188b521a3c2b6e0d2c
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-05-31 17:31:59 +0530

    Todo

--- Todo List/todo/src/App.js
+++ Todo List/todo/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import { Todo } from './Components/Todo';
+import { Balloon } from './Components/Balloon';
 
 function App() {
   return (
     <div className="App">
       <Todo/>
+      {/* <Balloon /> */}
     </div>
   );
 }
--- Todo List/todo/src/Components/Balloon.jsx
+++ Todo List/todo/src/Components/Balloon.jsx
@@ -4,6 +4,19 @@
 export const Balloon = () => {
 
     return(
-        
+        <>
+      <h1>BALLOON GAME</h1>
+      <div className="mainDiv">
+            <div className="left"></div>
+            <div className="right"></div>
+            <div className="input">
+                <input type="text" id="ballonnumber" placeholder="Enter Your Ballon Number"  /> <br />
+                <button>Shoot</button>
+            </div>
+        </div>
+
+          
+
+        </>
     )
 }
