c500dd5e31030c228235c5adcce03a8e51c00b17
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-06-01 19:46:03 +0530

    Use Ref

--- Todo List/todo/src/Components/Hooks/UseRefHook.jsx
+++ Todo List/todo/src/Components/Hooks/UseRefHook.jsx
@@ -6,6 +6,7 @@
 // 1. Accessing the  Dom Elements 
 // 2. Persist the values after successfull rendering   --------> (store previous values)
 
+import { useEffect } from "react";
 import { useState } from "react";
 import { useRef } from "react"
 
@@ -18,20 +19,28 @@ export const UseRefHook = () => {
 
 
     const [counter , setCounter] = useState(0)
-    const prev = useRef()
+    const prev = useRef()                                                      // before page rendered
+
+    useEffect(()=>{prev.current = counter},[counter])                          // after page rendered
     
 
     return(
-        <> 
+        <>
         
         <div className="todo">
 
         <input type="text" id="name" placeholder="enter your name" defaultValue={ref.current} ref={ref} />
         <button>Submit</button>
 
-        <h2>{counter}</h2>
+
+        <h2>Current Value : {counter}</h2>
+        <h2> Previous Value : {prev.current}</h2>
         <button onClick={()=>setCounter(counter-1)}>-</button>
         <button onClick={()=>setCounter(counter+1)}>+</button>
+
+
+        
+
         
         </div>
 
