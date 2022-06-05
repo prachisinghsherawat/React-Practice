3b6803fcbff2ea16abaa0eebf281af0c083a1171
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-06-05 13:05:29 +0530

    Practise

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+
 import { Todo } from './Components/Todo';
-import { Balloon } from './Components/Balloon';
 import { UseRefHook } from './Components/Hooks/UseRefHook';
 import { UseMemoHook } from './Components/Hooks/UseMemoHook';
 import { UseReducerHook } from './Components/Hooks/UseReducerHook';
 
 function App() {
   return (
+
     <div className="App">
       {/* <Todo/> */}
-      {/* <Balloon /> */}
       {/* <UseRefHook /> */}
       {/* <UseMemoHook /> */}
-      <UseReducerHook />
+      {/* <UseReducerHook /> */}
     </div>
+    
   );
 }
 
