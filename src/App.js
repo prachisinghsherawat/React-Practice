337f6644ffce59ea58a49e978ff8b9a6ebae2fb9
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-06-05 13:06:14 +0530

    Todo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
-import { Todo } from './Components/Todo';
+import { Todo } from './Components/Todo/Todo';
 import { UseRefHook } from './Components/Hooks/UseRefHook';
 import { UseMemoHook } from './Components/Hooks/UseMemoHook';
 import { UseReducerHook } from './Components/Hooks/UseReducerHook';
@@ -15,7 +15,7 @@ function App() {
       {/* <UseMemoHook /> */}
       {/* <UseReducerHook /> */}
     </div>
-    
+
   );
 }
 
