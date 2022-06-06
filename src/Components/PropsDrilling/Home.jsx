3736ac15fdae2a065ccdfd66b67f5cfa16dceb92
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-06-07 00:23:52 +0530

    Home

--- src/Components/PropsDrilling/Home.jsx
+++ src/Components/PropsDrilling/Home.jsx
@@ -1,11 +1,21 @@
+import axios from "axios";
+import { useEffect } from "react";
 import { useState } from "react"
 
 
 export const Home = () => {
 
-    const [data,setData] = useState([])
+    const [movieData,setMovieData] = useState([]);
+
+    useEffect(() => {getData()},[])      
+
+    const getData = () => {
+
+        axios.get("http://localhost:8080/movies").then((res)=> setMovieData(res.data))
+    }
 
     return(
+
         <>
 
         <div className="nav">
@@ -14,6 +24,16 @@ export const Home = () => {
             <li>Action</li>
             <li>History</li>
             <li>Romantic</li>
+        </div>
+
+        <div className="homePage">
+            {movieData.map((el)=>(
+
+                <div className="imgBox">
+                    <img id="imgIs" src={el.img}/>
+                </div>
+        
+            ))}
         </div>
 
         </>
