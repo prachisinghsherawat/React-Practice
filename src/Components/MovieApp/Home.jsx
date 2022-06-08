43cca6f982f1a364e965fe8d9ac48f1f07b4cea9
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-06-08 20:47:18 +0530

    Home

--- src/Components/MovieApp/Home.jsx
+++ src/Components/MovieApp/Home.jsx
@@ -6,24 +6,44 @@ import { useState } from "react"
 export const Home = () => {
 
     const [movieData,setMovieData] = useState([]);
+    const [movie , setMovie] = useState("")
 
-    useEffect(() => {getData()},[])      
+    useEffect(() => {getData()},[movie]) 
 
-    const getData = () => {
 
-        axios.get("http://localhost:8080/movies").then((res)=> setMovieData(res.data))
+    
+
+    // ------------------------ Using Backend ----------------------------------------------------------->
+
+
+
+    // const getData = () => {
+
+    //     if(movie == ""){
+    //         axios.get(`http://localhost:8080/movies`).then((res)=> setMovieData(res.data))
+    //     }
+    //     else{
+    //         axios.get(`http://localhost:8080/movies?type=${movie}`).then((res)=> setMovieData(res.data))
+    //     }
+        
+    // }
+
+    const MovieCheck = (el) => {
+        setMovie(el)
     }
+
 
     return(
 
         <>
 
         <div className="nav">
-            <li>Comedy</li>
-            <li>Thrill</li>
-            <li>Action</li>
-            <li>History</li>
-            <li>Romantic</li>
+            <li onClick={() => MovieCheck("")}>Home</li>
+            <li onClick={() => MovieCheck("comedy")}>Comedy</li>
+            <li onClick={() => MovieCheck("thrilled")}>Thrilled</li>
+            <li onClick={() => MovieCheck("action")}>Action</li>
+            <li onClick={() => MovieCheck("history")}>History</li>
+            <li onClick={() => MovieCheck("romantic")}>Romantic</li>
         </div>
 
         <div className="homePage">
