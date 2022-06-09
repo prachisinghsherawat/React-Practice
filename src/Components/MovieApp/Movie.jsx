4c2e4741cf361c83c06c4f9cd5504936974276f1
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-06-09 12:29:05 +0530

    Movies

---
+++ src/Components/MovieApp/Movie.jsx
@@ -0,0 +1,21 @@
+
+
+
+export const Movies = () => {
+
+    return(
+
+        <>
+        
+        <div className="nav">
+            <li>HOME</li>
+            <li>Thriller</li>
+            <li>Comedy</li>
+            <li>Action</li>
+            <li>Romantic</li>
+            <li>History</li>
+        </div>
+        </>
+        
+    )
+}
