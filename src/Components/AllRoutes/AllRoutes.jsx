6516b0899556083d14915a3f9f624f687bef4961
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-06-09 12:35:49 +0530

    All Routes

--- src/Components/AllRoutes/AllRoutes.jsx
+++ src/Components/AllRoutes/AllRoutes.jsx
@@ -1,6 +1,7 @@
 import { Home } from "../MovieApp/Home"
 import { HomeDetails } from "../MovieApp/HomeDetails"
 import { Route , Routes } from "react-router"
+import { Movies } from "../MovieApp/Movie"
 
 export const AllRoutes = () => {
 
@@ -9,6 +10,7 @@ export const AllRoutes = () => {
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/details" element={<HomeDetails />} />
+            <Route path="/movies" element={<Movies /> } />
         </Routes>
     )
 }
