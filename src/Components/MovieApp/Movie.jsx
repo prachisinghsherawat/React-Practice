6e30bbc75789f58d4cc48184da000bd2564bc3e4
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-06-09 18:06:54 +0530

    Movies

--- src/Components/MovieApp/Movie.jsx
+++ src/Components/MovieApp/Movie.jsx
@@ -1,7 +1,18 @@
+import axios from "axios"
+import { useEffect } from "react"
 
 
 
 export const Movies = () => {
+
+    const [movData , setMovData] =([])
+
+    useEffect(() => { getData()},[])
+
+    const getData = () => {
+
+        axios.get("http://localhost:8080/movies").then((res)=> setMovData(res.data))
+    }
 
     return(
 
@@ -14,6 +25,14 @@ export const Movies = () => {
             <li>Action</li>
             <li>Romantic</li>
             <li>History</li>
+        </div>
+
+        <div>
+            {movData.map((el) =>(
+                <div>
+                    <img src={el.img} />
+                </div>
+            ))}
         </div>
 
         
