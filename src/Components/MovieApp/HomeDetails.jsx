84d80e4650d12e013309c5661696af09f30b535e
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-06-10 11:31:04 +0530

    Home Details

--- src/Components/MovieApp/HomeDetails.jsx
+++ src/Components/MovieApp/HomeDetails.jsx
@@ -1,10 +1,24 @@
+import axios from "axios"
+import { useState } from "react"
+import { useEffect } from "react"
 import { useParams } from "react-router"
 
 
 
 export const HomeDetails = () => {
 
+    const [details , setDetails] = useState({})
+
     const {id} = useParams()
+    useEffect(()=>{getData()},[])
+
+    const getData = () => {
+
+        axios.get(`http://localhost:8080/movies/${id}`).then((res)=> setDetails(res.data))
+    }
+
+    console.log(details)
+
 
     return(
         
@@ -12,10 +26,17 @@ export const HomeDetails = () => {
         <div className="flexDiv">
 
             <div className="left">
-                <div className="bigImg"></div>
+                <div className="bigImg">
+                    <img id="imgIs" src={details.img} />
+                </div>
             </div>
 
-            <div className="right"></div>
+            <div className="right">
+                <h1>{details.name}</h1>
+                <p>{details.type}</p>
+                <h3>{details.rating}</h3>
+                
+            </div>
             
         </div>
         </>
