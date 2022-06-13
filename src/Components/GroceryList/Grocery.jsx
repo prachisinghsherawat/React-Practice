7d1fffd5870b04e14ccd05e05a5f22f76727e098
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-06-13 23:25:33 +0530

    Grocery List

--- src/Components/GroceryList/Grocery.jsx
+++ src/Components/GroceryList/Grocery.jsx
@@ -3,22 +3,59 @@ import { useState } from "react"
 
 export const Grocery = () => {
 
+
+    
+    const [ arr , setArr] = useState([])
+
     const [item , setItem] = useState({
         food : "" ,
-        status : false
-    })
-    
+        status : "false"
+    })  
+
+
+    const HandleChange = (e) => {
+
+        const {id , value} = e.target ;
+        setItem({...item , [id] : value})
+    }
+
 
     const HandleSubmit = () => {
+        setArr([...arr , item])
+        document.getElementById("food").value = " "
+    }
+
+
+    const Delete = (index) => {
+        arr.splice(index,1)
+        setArr([...arr])
+    }
+
+    const setStatus = () => {
 
     }
 
+
     return(
         <>
+           
+           <div className="todo">
+                <h1>Grocery List</h1>
+                <input type="text" id="food" placeholder="enter your food item" onChange={HandleChange} />
 
-            <input type="text" id="food" placeholder="enter your food item" onChange={(e)=> setItem(e.target.value)}/>
-            <button>Submit</button>
+                <button onClick={HandleSubmit}>Submit</button>
 
+
+                {arr.map((el , index) => (
+                    
+                   <div>
+                        {index+1}  {el.food}  <button onClick={setStatus}>{el.status}</button>  <button onClick={()=>{Delete(index)}}>Delete</button>
+                    </div>
+                ))}
+           
+           </div>
+
+           
         </>
     )
 }
