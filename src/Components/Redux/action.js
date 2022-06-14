c3e4d7a0e364efbe52a8e57cdc8575bbf28398cb
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-06-14 20:44:35 +0530

    Redux

---
+++ src/Components/Redux/action.js
@@ -0,0 +1,18 @@
+
+
+export const ADD_TODO = "ADD_TODO"
+export const GET_TODO = "GET_TODO"
+
+
+const AddTodo = () => ({
+    type : ADD_TODO          
+})
+
+const GetTodo = () => ({
+    type : GET_TODO ,
+    payload : payload
+})
+
+export const HandleSubmit = () => (dispatch) => {
+    
+}
--- src/Components/ReduxTodo/ReduxTodo.jsx
+++ src/Components/ReduxTodo/ReduxTodo.jsx
@@ -3,6 +3,17 @@
 export const ReduxTodo = () => {
 
     return(
-        <></>
+
+
+        <>
+
+        <div className="todo">
+
+            <input type="text" placeholder="add your todo"/>
+            <button>Submit</button>
+
+        </div>
+
+        </>
     )
 }
