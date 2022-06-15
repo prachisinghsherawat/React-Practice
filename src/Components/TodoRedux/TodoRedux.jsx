5d51cc63c784f3a1c24a229a7e8ce903d002ffc4
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-06-15 15:32:50 +0530

    Todo React

--- src/Components/TodoRedux/TodoRedux.jsx
+++ src/Components/TodoRedux/TodoRedux.jsx
@@ -2,8 +2,21 @@
 
 export const TodoRedux = () => {
 
+    const [items ,setItems] = useState({
+
+        food : "",
+        status : false
+    })
+
+    
+
     return(
         
-        <></>
+        <>
+
+        <input type="text" placeholder="enter a todo" id="food" />
+        <button>Submit</button>
+
+        </>
     )
 }
