b93a81cd9c92113ec18e86367b784f87d556fa4b
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-06-15 22:44:09 +0530

    action

--- src/Components/Redux/action.js
+++ src/Components/Redux/action.js
@@ -1,20 +1,26 @@
-// import axios from "axios"
+import axios from "axios"
 
 
-// export const ADD_TODO = "ADD_TODO"
-// export const GET_TODO = "GET_TODO"
+export const ADD_TODO = "ADD_TODO"
+export const GET_TODO = "GET_TODO"
 
 
-// const AddTodo = () => ({
-//     type : ADD_TODO          
-// })
+const AddTodo = () => ({
+    type : ADD_TODO          
+})
 
-// const GetTodo = () => ({
-//     type : GET_TODO ,
-//     payload : payload
-// })
+const GetTodo = (payload) => ({
 
-// export const HandleSubmit = () => (dispatch) => {
+    type : GET_TODO ,
+    payload : payload
+})
 
-//     axios.post("http://localhost:8080/todos")
-// }
+export const HandleSubmit = (data) => (dispatch) => {
+
+    axios.post("http://localhost:8080/todos" , data ).then(() => getData())
+}
+
+export const getData = () => (dispatch) => {
+
+    axios.get("http://localhost:8080/todos" ).then((res) => dispatch(GetTodo(res.data)))
+}
---
+++ src/Components/TodoRedux/TodoRedux.jsx
@@ -0,0 +1,19 @@
+
+
+
+export const TodoRedux = () => {
+
+    return(
+
+        <>
+
+        <div className="todo">
+
+            <input type="text" placeholder="enter the todo" id="todo" />
+            <button>Submit</button>
+            
+        </div>
+
+        </>
+    )
+}
