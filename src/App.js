ba08b35608929e0674365e7f851765100efe6c49
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-06-20 13:34:40 +0530

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-
-// import { Todo } from './Components/Todo/Todo';
-// import { UseRefHook } from './Components/Hooks/UseRefHook';
-// import { UseMemoHook } from './Components/Hooks/UseMemoHook';
-// import { UseReducerHook } from './Components/Hooks/UseReducerHook';
-import { Home } from './Components/MovieApp/Home';
-import { Footer } from './Components/Footer/Footer';
 import { AllRoutes } from './Components/AllRoutes/AllRoutes';
+import { Navbar } from './Components/Navbar/Navbar';
 
 function App() {
   
@@ -19,6 +13,7 @@ function App() {
       {/* <UseMemoHook /> */}
       {/* <UseReducerHook /> */}
 
+      <Navbar />
       <AllRoutes />
     </div>
 
