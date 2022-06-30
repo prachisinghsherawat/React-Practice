1d09ab0df18f22bb87a74a426294f22e2a8b1fc2
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-06-30 14:23:04 +0530

    Task 6

--- src/Components/Revision/Task6.jsx
+++ src/Components/Revision/Task6.jsx
@@ -1,9 +1,31 @@
 
-// www.themealdb.com/api/json/v1/1/categories.php
+import axios from "axios"
+import { useEffect } from "react"
+import { useState } from "react"
+
 export const Task6 = () => {
+
+    const [data , setData] = useState([])
+    useEffect(() => {getData()},[])
+
+    const getData = () => {
+        axios.get("https://www.themealdb.com/api/json/v1/1/categories.php").then((res) => setData(res.data.categories))
+    }
+    console.log(data)
 
     return(
 
-        <></>
+        <>
+        <div className="grid">
+        {data.map((el) => (
+            <div>
+
+                <img src={el.strCategoryThumb} />
+                <h1>{el.strCategory}</h1>
+
+            </div>
+        ))}
+        </div>
+        </>
     )
 }
