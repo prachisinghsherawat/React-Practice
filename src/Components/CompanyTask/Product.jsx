5082c35db593036b39c4120a3ddc13f36263af5b
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-06-30 22:50:40 +0530

    Products

--- src/Components/CompanyTask/Product.jsx
+++ src/Components/CompanyTask/Product.jsx
@@ -5,30 +5,34 @@ export const Product = () => {
 
     let products = [
 
-        { id:1 , name : "Product-1" , price :100},
-        { id:2 , name : "Product-2" , price :200},
-        { id:3 , name : "Product-3" , price :300},
-        { id:4 , name : "Product-4" , price :400}
+        { id:1 , name : "Product-1" , price :100 },
+        { id:2 , name : "Product-2" , price :200 },
+        { id:3 , name : "Product-3" , price :300 },
+        { id:4 , name : "Product-4" , price :400 }
     ]
 
     return(
         
         <>
 
-        <h1>Products</h1>
+        <div className="Products">
 
-        {products.map((el)=>(
-            <div>
-                {el.name}
-                {el.price}
+            <h1>Products</h1>
+
+            {products.map((el)=>(
+
+                <div id="box">
+                    <p>{el.name}</p>
+                    <p>{el.price}</p>
                 
-                <div>
-                    <button>-</button>
-                    <p>{}</p>
-                    <button>+</button>
+                    <div>
+                        <button>-</button>
+                        <button>+</button>
+                    </div>
                 </div>
-            </div>
-        ))}
+            ))}
+        </div>
+
         </>
     )
 }
