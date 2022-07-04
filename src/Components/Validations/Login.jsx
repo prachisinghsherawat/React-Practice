c242eb0363cae97494b7ae4883796df7b3a72167
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-07-05 00:03:12 +0530

    Sign Up

--- src/Components/Validations/Login.jsx
+++ src/Components/Validations/Login.jsx
@@ -2,10 +2,19 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 
 export function Login() {
+
   return (
-    <Box
+
+    <>
+
+    <div className="todo">
+      
+    <h1>Sign Up</h1>
+    <Box id=''
       component="form"
       sx={{
         '& > :not(style)': { m: 1, width: '25ch' },
@@ -13,10 +22,20 @@ export function Login() {
       noValidate
       autoComplete="off"
     >
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      <TextField id="name" label="Name" variant="outlined" />
+      <TextField id="email" label="Email" variant="outlined" />
+      <TextField id="password" label="Password" variant="outlined" />
+
+    
+      <Stack spacing={2} direction="row">
+        <Button variant="contained">SUBMIT HERE</Button>
+      </Stack>
+
+
       
     </Box>
+    </div>
+
+    </>
   );
 }
--- src/Components/Validations/SignUp.jsx
+++ src/Components/Validations/SignUp.jsx
@@ -2,9 +2,61 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import { useState } from 'react';
 
 export function SignUp() {
+
+  const [signup, setSignup] = React.useState({
+
+    name : "",
+    email : "",
+    password : ""
+
+  })
+
+  const [errors , setErrors] = useState({})
+  const [submitData , setSubmitData] = useState([])
+
+
+  const HandleChange = (e) =>{
+      
+    const {id,value} = e.target;
+    setSignup({...signup , [id]:value})
+  }
+
+  const HandleSubmit = () => {
+
+    setErrors(Valid(signup))
+    setSubmitData([...submitData , signup])
+  }
+
+  const Valid = (val) => {
+
+    const err = {}
+
+    if(!val.name){
+      err.name = "name is required"
+    }
+    if(!val.email){
+      err.email = "email is required"
+    }
+    if(!val.password){
+      err.password = "password is required"
+    }
+    return err
+  }
+  
+
   return (
+
+    <>
+
+    <div className="todo">
+
+    <h1>Sign Up</h1>
+
     <Box id=''
       component="form"
       sx={{
@@ -13,10 +65,20 @@ export function SignUp() {
       noValidate
       autoComplete="off"
     >
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      <TextField id="name" label="Name" variant="outlined" onChange={HandleChange} />
+      <TextField id="email" label="Email" variant="outlined" onChange={HandleChange} />
+      <TextField id="password" label="Password" variant="outlined" onChange={HandleChange} />
+
+    
+      <Stack spacing={2} direction="row">
+        <Button variant="contained">SUBMIT HERE</Button>
+      </Stack>
+
+
       
     </Box>
+    </div>
+
+    </>
   );
 }
