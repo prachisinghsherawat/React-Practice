4a16064f265f1141a7470e0025d58b56445ad99c
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-07-07 00:56:56 +0530

    Product

---
+++ src/Components/CompanyTask/Cart.jsx
@@ -0,0 +1,39 @@
+
+
+
+export const Cart = () => {
+
+    return(
+        <>
+
+        <div className="cart">
+            <h1>Cart</h1>
+
+                {item.map((el)=>(
+                    <div>
+                        {el.quantity >0 ?
+                          <div id="box">
+                            <p>{el.name}</p>
+                            <p>{el.quantity*el.price}</p>
+                          </div>
+
+                          :
+
+                          <div>
+                              <p>Nothing in Cart</p>
+                          </div>
+                        
+                        }                        
+                    </div>
+                    
+                ))}
+
+            <div>
+                <h3>Total : {total}</h3>
+            </div>
+
+        </div>
+        
+        </>
+    )
+}
--- src/Components/CompanyTask/Product.jsx
+++ src/Components/CompanyTask/Product.jsx
@@ -69,33 +69,7 @@ export const Product = () => {
 
             </div>
 
-            <div className="cart">
-                <h1>Cart</h1>
-
-                {item.map((el)=>(
-                    <div>
-                        {el.quantity >0 ?
-                          <div id="box">
-                            <p>{el.name}</p>
-                            <p>{el.quantity*el.price}</p>
-                          </div>
-
-                          :
-
-                          <div>
-                              <p>Nothing in Cart</p>
-                          </div>
-                        
-                        }                        
-                    </div>
-                    
-                ))}
-
-                <div>
-                    <h3>Total : {total}</h3>
-                </div>
-
-            </div>
+            <Cart item={item} />
 
         </div>
         </>
