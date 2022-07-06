c8be321cfbb8e8d69c2277d8497526675d518799
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-07-06 14:49:41 +0530

    Random

--- src/Components/RandomColor/RandomColor.jsx
+++ src/Components/RandomColor/RandomColor.jsx
@@ -4,13 +4,23 @@ import { useState } from "react"
 
 export const RandomColour = () => {
 
-    const [random , setRandom] = useState()
+    const [random , setRandom] = useState("")
+
+    const setRandomColor = () => {
+
+        var randomColor = Math.floor(Math.random()*16777215).toString(16);
+        setRandom(randomColor)
+
+        document.getElementById("color").style.backgroundColor = "#" + random
+    }
+    console.log(random)
 
     return(
 
         <>
             <div className="colorBox">
-                <div></div>
+
+                <div id="color" onClick={setRandomColor}></div>
                 <div></div>
             </div>
         </>
