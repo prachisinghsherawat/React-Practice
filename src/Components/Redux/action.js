5d369b4eeacf342ce939db0976edbc7d3028621e
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-07-13 00:14:09 +0530

    Ction

---
+++ src/Components/Redux/action.js
@@ -0,0 +1,16 @@
+import axios from "axios"
+
+
+const GET_BOOKS = "GET_BOOKS"
+
+
+const getBookData = (payload)=> ({
+
+    type : GET_BOOKS ,
+    payload : payload
+})
+
+const getData = () => (dispatch) => {
+
+    axios.get("https://62a89ad6943591102ba66ccd.mockapi.io/api/v1/books").then((res) => dispatch(getBookData(res.data)))
+}
