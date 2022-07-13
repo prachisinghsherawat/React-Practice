b766f8191685a0e23e522efa7e461769d3ea067b
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-07-13 13:16:30 +0530

    Redux

--- src/Components/Redux/action.js
+++ src/Components/Redux/action.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 
-const GET_BOOKS = "GET_BOOKS"
+export const GET_BOOKS = "GET_BOOKS"
 
 
 const getBookData = (payload)=> ({
@@ -10,7 +10,7 @@ const getBookData = (payload)=> ({
     payload : payload
 })
 
-const getData = () => (dispatch) => {
+export const getData = () => (dispatch) => {
 
     axios.get("https://62a89ad6943591102ba66ccd.mockapi.io/api/v1/books").then((res) => dispatch(getBookData(res.data)))
 }
--- src/Components/Redux/reducer.js
+++ src/Components/Redux/reducer.js
@@ -1,2 +1,17 @@
+import { GET_BOOKS } from "./action"
 
 
+
+const initState = {
+
+    Books : []
+}
+
+const BookReducer = (store = initState , {type , payload}) => {
+
+    switch(type){
+
+        case GET_BOOKS :
+            return {...store , Books : [...payload]}
+    }
+}
