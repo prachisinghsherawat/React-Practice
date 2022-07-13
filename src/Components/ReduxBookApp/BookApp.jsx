61134a4223c776d786713a544a69ec97bdc278ff
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-07-13 13:41:53 +0530

    Book App

--- src/Components/ReduxBookApp/BookApp.jsx
+++ src/Components/ReduxBookApp/BookApp.jsx
@@ -1,8 +1,18 @@
 
 // https://62a89ad6943591102ba66ccd.mockapi.io/api/v1/books
 
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { getData } from "../Redux/action"
+
 
 export const BookApp = () => {
+
+    const dispatch = useDispatch()
+    useEffect(()=> {dispatch(getData())},[])
+
+    const BookDetails = useSelector((Store)=> Store.Book.Books)                  // Store ---> Reducer
+    console.log(BookDetails)           
 
     return(
 
