d285b9a52057a94d39b2011452af418dd300494e
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-08-25 18:58:57 +0530

    MyPractise

---
+++ src/Components/MyPractise/MyPractise.jsx
@@ -0,0 +1,17 @@
+
+
+export const MyPractise = () => {
+
+    let products = [
+
+        { id:1 , name : "Product-1" , price :100 , quantity : 0},
+        { id:2 , name : "Product-2" , price :200 , quantity : 0},
+        { id:3 , name : "Product-3" , price :300 , quantity : 0}
+    ]
+
+    return(
+
+        <>
+        </>
+    )
+}
